143d7830f66f8783ea8ec48872125fceb22b774f
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-12 11:33:11 +0800

    #5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>

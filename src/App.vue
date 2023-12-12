b1c6f10913263c80d14ac8f4d32c7bfdfa87cf35
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-12 11:48:26 +0800

    #7

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
-    <router-view></router-view>
   </div>
 </template>
 
